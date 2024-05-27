66d963cd3fbe4e272e7e6cb2004136cd58bdcc1c
Zh0r1k <ZhonamMax@gmail.com>
2024-05-27 21:54:23 +0300

    updateeeee

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,6 @@
-export const BASE_URL = 'https://frontend-zhuravlevilya.nomoredomainswork.ru'
+export const BASE_URL = [    'http://localhost:3001',
+'http://pindie-zhuravlevilya.nomoredomainswork.ru',
+  'https://pindie-zhuravlevilya.nomoredomainswork.ru']
 export const endpoints = {
   games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/login`,
