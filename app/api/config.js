8eed0da606e23b59eb16f08f63bd17f7313c9785
Zh0r1k <ZhonamMax@gmail.com>
2024-05-28 15:25:27 +0300

    hbbbb

--- app/api/config.js
+++ app/api/config.js
@@ -1,6 +1,7 @@
-export const BASE_URL = [    'http://localhost:3001',
-'http://pindie-zhuravlevilya.nomoredomainswork.ru',
-  'https://pindie-zhuravlevilya.nomoredomainswork.ru']
+export const BASE_URL = ['https://pindie-zhuravlevilya.nomoredomainswork.ru/api',
+'https://pindie-zhuravlevilya.nomoredomainswork.ru/api',
+'http://localhost:3001/api'
+]
 export const endpoints = {
   games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/login`,
