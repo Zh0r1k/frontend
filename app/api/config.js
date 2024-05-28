36691a05a72992287d04a94c57456e39688249a4
Zh0r1k <ZhonamMax@gmail.com>
2024-05-28 16:52:35 +0300

    gggggggggggggggggggggg

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = ["localhost:3001/api", 
+export const BASE_URL = ["http://localhost:3001/api", 
 "https://pindie-zhuravlevilya.nomoredomainswork.ru/api",
   "http://pindie-zhuravlevilya.nomoredomainswork.ru/api"]
 
